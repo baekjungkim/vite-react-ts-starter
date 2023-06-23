28b3e1cdb4f4878b22c6c94b632cdf158d017f52
Kim Baekjung <zvgandam@gmail.com>
2023-06-23 13:03:48 +0900

    Structure and Organize

---
+++ src/pages/About/index.tsx
@@ -0,0 +1,14 @@
+import AboutSection from './components/AboutSection';
+
+function AboutPage() {
+  return (
+    <>
+      <h2>About Page!</h2>
+      <AboutSection />
+      <AboutSection />
+      <AboutSection />
+    </>
+  );
+}
+
+export default AboutPage;
