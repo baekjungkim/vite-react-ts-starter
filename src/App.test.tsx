bd88b4ef5082d1853158c2382208e73b63fb83f0
Kim Baekjung <zvgandam@gmail.com>
2023-07-05 14:32:08 +0900

    husky lint error modify

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
-import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
 import { MemoryRouter } from 'react-router-dom';
+import { describe, it } from 'vitest';
+
 import { WrappedApp, App } from './App';
 
 describe('App', () => {
